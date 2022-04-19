293b16d5b7ce527af3f70484c61e3ca4d4574d2c
”hirohitoqwe” <balduin@internet.ru>
2022-04-19 23:18:03 +0300

    normal

--- handler.php
+++ handler.php
@@ -5,16 +5,27 @@ $nameOfFile=$_POST['filename'];
 $forload=$nameOfFile.'.'.'xlsx';
 $excel=PHPExcel_IOFactory::load("$forload");
 $firstsymb='A';
+$allsubj=[];
 $second=chr(ord($firstsymb)+1);//Хорошая конструкция
 for ($i=0;$i<7;$i++){
     $cellid=1;
     $value=1;
-    while($value!=''){
+    $val=1;
+    
+    $subj=[];
+    while($val!=''){
         $value=$excel->getActiveSheet()->getCell($firstsymb.$cellid);
-        echo $firstsymb.' - '.$cellid.'</br>';
-        echo $value.'</br>';
+        $val=$value->getValue();
+        echo $val.'</br>';
+        $subj[]=$val;
         $cellid++;
     }
+    $allsubj[]=$subj;
     $firstsymb=chr(ord($firstsymb)+1);
+}
+foreach ($allsubj as $key=>$value){
+    foreach($value as $supervalue){
+        echo $supervalue.'</br>';
+    }
 }
 ?>
